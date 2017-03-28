6ed1823337db343e238081f6cdcd03f10148c9b7
Amrut Kulkarni <amrutKulkarni@lawn-128-61-125-130.lawn.gatech.edu>
2017-03-28 17:59:19 -0400

    Updated some files, still working on application

--- src/soundboard/AppScreen.java
+++ src/soundboard/AppScreen.java
@@ -3,6 +3,7 @@ package soundboard;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * Created by amrutKulkarni on 3/28/17.
@@ -11,11 +12,13 @@ public class AppScreen extends BorderPane{
     private AudioPlayer rpgPlayer = new AudioPlayer();
     private Button burp;
     private Button magic1;
+    private Text rightText;
     private VBox buttonHolder;
 
     public AppScreen() {
         burp = new Button("Burp");
         magic1 = new Button("Magic1");
+        rightText = new Text("Magic");
         burp.setOnAction(e -> {
                 rpgPlayer.playBurp();
             });
@@ -24,8 +27,10 @@ public class AppScreen extends BorderPane{
         });
         buttonHolder = new VBox(10);
         buttonHolder.getChildren().add(burp);
+        buttonHolder.getChildren().add(rightText);
         buttonHolder.getChildren().add(magic1);
         this.setCenter(burp);
+        this.setRight(rightText);
         this.setRight(magic1);
     }
 }
--- src/soundboard/RPGSoundboard.java
+++ src/soundboard/RPGSoundboard.java
@@ -5,8 +5,6 @@ package soundboard;
  */
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
